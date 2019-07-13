e4c8dc53c061f9d0b7c48c90f24289638d2aefa0
yoshikimasubuchi <yoshikimasubuchi@chao-labnoMacBook-puro.local>
2019-07-13 15:41:32 +0900

    users-id-pageを導入

---
+++ pages/users/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+     User ID {{userId}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id
+    }
+  }
+}
+</script>
